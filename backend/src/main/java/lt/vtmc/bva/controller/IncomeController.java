507b7bf0d003c992098d50d58eab5232e48917ad
Minte <minte.jarmalaviciute@gmail.com>
2022-04-27 19:11:13 +0300

    atnaujintas backas

--- backend/src/main/java/lt/vtmc/bva/controller/IncomeController.java
+++ backend/src/main/java/lt/vtmc/bva/controller/IncomeController.java
@@ -1,14 +1,19 @@
 package lt.vtmc.bva.controller;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import lt.vtmc.bva.model.Income;
 import lt.vtmc.bva.repository.IncomeRepository;
 
@@ -20,20 +25,37 @@ public class IncomeController {
 	@Autowired
 	public IncomeRepository incomeRepo;
 
-//    public String showform(Model m){    
-//        m.addAttribute("command", new Income());  
-//        return "empform"; 
-//    } 
-
 	@GetMapping
 	public List<Income> getIncomes() {
 		return incomeRepo.findAll();
 
 	}
+
+	@GetMapping("/{id}")
+	public Income getIncome(@PathVariable Long id) {
+		return incomeRepo.findById(id).get();
+	}
+
 	@PostMapping
-	
-	public Income postIncomes(@RequestBody Income income){
+	public Income postIncomes(@RequestBody Income income) {
 		return incomeRepo.save(income);
 	}
+
+	@DeleteMapping("/{id}")
+	@RequestMapping
+	public void deleteIncomes(@PathVariable Long id) {
+		incomeRepo.deleteById(id);
+	}
 	
+//	ResponseEntity
+	@PutMapping("/{id}")
+	public ResponseEntity<Income> updateIncomes(@PathVariable Long id, @Valid @RequestBody Income incomeDetails) {
+		Income income = incomeRepo.findById(id).orElseThrow() ;
+		
+		income.setSum(incomeDetails.getSum());
+		income.setDate(incomeDetails.getDate());
+		income.setComment(incomeDetails.getComment());
+		final Income updatedIncome = incomeRepo.save(income);
+        return ResponseEntity.ok(updatedIncome);
+	}
 }
--- backend/src/main/java/lt/vtmc/bva/model/Income.java
+++ backend/src/main/java/lt/vtmc/bva/model/Income.java
@@ -1,8 +1,11 @@
 package lt.vtmc.bva.model;
+
 import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.Data;
@@ -13,23 +16,19 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Entity
 public class Income {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
 	private BigDecimal sum;
+	@NotNull
 	private LocalDate date;
 	private String comment;
 
-	
 	private Income(BigDecimal sum, LocalDate date, String comment) {
-		this.sum=sum;
-		this.date=date;
-		this.comment=comment;
+		this.sum = sum;
+		this.date = date;
+		this.comment = comment;
 	}
-	
-	
-	
-	
 	
 }
